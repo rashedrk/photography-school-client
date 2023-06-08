8716357ec3576d2515b1191b320c056b8761ce8a
rashedrk <rashedrk001@gmail.com>
2023-06-08 23:17:10 +0600

    navbar added

---
+++ src/Layout/Main.jsx
@@ -0,0 +1,11 @@
+import Navbar from "../pages/Shared/Navbar/Navbar";
+
+const Main = () => {
+    return (
+        <>
+            <Navbar></Navbar>
+        </>
+    );
+};
+
+export default Main;
