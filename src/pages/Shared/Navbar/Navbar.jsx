9a45c5f506b9be65bccef02bbce4fd34552d8440
rashedrk <rashedrk001@gmail.com>
2023-06-09 16:21:42 +0600

    navbar updated with profile pic and logot option added

--- src/pages/Shared/Navbar/Navbar.jsx
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -1,7 +1,14 @@
 import { Link } from "react-router-dom";
+import useAuth from "../../../hooks/useAuth";
 
 
 const Navbar = () => {
+    const { user, logout } = useAuth();
+    const handleLogout = () => {
+        logout()
+        .then(() => {})
+        .catch(err => console.log(err))
+    }
     //navbar options
     const NavItems = <>
         <li><Link to="/">Home</Link></li>
@@ -29,18 +36,25 @@ const Navbar = () => {
                 </ul>
             </div>
             <div className="navbar-end">
-                <label tabIndex={0} className="btn btn-ghost btn-circle">
+                <label  className="btn btn-ghost btn-circle">
                     <div className="indicator">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
                         <span className="badge badge-sm indicator-item">8</span>
                     </div>
                 </label>
-                <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-                    <div className="w-10 rounded-full">
-                        <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
-                    </div>
-                </label>
-                <Link to="/login" className="btn primary-btn">Login</Link>
+                {
+                    user ? <>
+                        <label className="btn btn-ghost btn-circle avatar tooltip tooltip-bottom" data-tip={user.displayName}>
+                            <div className="w-10 rounded-full">
+                                <img src={user.photoURL} />
+                            </div>
+                        </label>
+                        <button onClick={handleLogout} className="btn btn-xs ms-2 -mt-1 primary-btn">Log out</button>
+                    </>
+                    : <Link to="/login" className="btn primary-btn">Login</Link>
+                }
+
+                
             </div>
         </div>
     );
