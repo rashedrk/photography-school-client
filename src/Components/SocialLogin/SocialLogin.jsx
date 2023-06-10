a6280e8ee45c154e7d0e7248d4081dfcc5e0ab15
rashedrk <rashedrk001@gmail.com>
2023-06-10 12:54:18 +0600

    registration page add user to db

--- src/Components/SocialLogin/SocialLogin.jsx
+++ src/Components/SocialLogin/SocialLogin.jsx
@@ -1,6 +1,8 @@
 import { FcGoogle } from 'react-icons/fc';
 
 const SocialLogin = () => {
+    
+
     return (
 
         <button className="mb-2 btn  rounded-2xl"><FcGoogle className='text-2xl'/>Login With Google</button>
